cc2a02886d77859acf7072ab8ef2accc85cf6af4
Manas-Dikshit <manasdikshit48@gmail.com>
2026-07-08 15:54:24 +0530

    Implement atomic lock acquisition using Redis SET NX PX

--- redis_lock.py
+++ redis_lock.py
@@ -1,16 +1,19 @@
 import uuid
-
 from config import redis_client
 
 
 class RedisLock:
-
     def __init__(self, lock_name, timeout=10000):
-
         self.lock_name = lock_name
-
         self.timeout = timeout
-
         self.owner_id = str(uuid.uuid4())
-
         self.redis = redis_client
+
+    def acquire(self):
+        acquired = self.redis.set(
+            name=self.lock_name,
+            value=self.owner_id,
+            nx=True,   # Only set if not exists
+            px=self.timeout  # Expire after timeout (ms)
+        )
+        return acquired is True
--- test_lock.py
+++ test_lock.py
@@ -1,9 +1,37 @@
+# from redis_lock import RedisLock
+
+# lock = RedisLock("inventory_lock")
+
+# print("Lock Name :", lock.lock_name)
+
+# print("Owner ID  :", lock.owner_id)
+
+# print("Timeout   :", lock.timeout)
+
+
+
+
+
+# from redis_lock import RedisLock
+
+# lock = RedisLock("inventory_lock")
+
+# if lock.acquire():
+#     print("Lock acquired successfully!")
+# else:
+#     print("Failed to acquire lock.")
+
+
+
+
 from redis_lock import RedisLock
+import time
 
 lock = RedisLock("inventory_lock")
 
-print("Lock Name :", lock.lock_name)
-
-print("Owner ID  :", lock.owner_id)
-
-print("Timeout   :", lock.timeout)
+if lock.acquire():
+    print("Lock acquired successfully!")
+    print("Sleeping for 20 seconds...")
+    time.sleep(20)
+else:
+    print("Failed to acquire lock.")
